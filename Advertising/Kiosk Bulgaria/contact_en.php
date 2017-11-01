e4c341a42d8553d0e66240798b9587d228357268
VelinSkenderov <velin.skenderov@bmsys.eu>
2017-11-01 14:43:03 +0200

    Add files via upload

---
+++ Advertising/Kiosk Bulgaria/contact_en.php
@@ -0,0 +1,291 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
+<head>
+<title>Kiosk Bulgaria - Touch screen kiosks, Digital signage, Kiosk software - Contact</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta content="kiosk,kiosks,kioski,sale,KIOSKY,selling,public computer,internet,terminal,bulgaria,information,terminals,saw,touchscreen,touch,screen,tft,web,internet kiosk,metal keyboard,trackball,contact us" name="keywords">
+<meta content="киоск,киоски,продажба,киоскай,обществен компютър,интернет,терминал,българия,информационни,терминали,акустични вълни,тъчскрийн,уеб,интернет киоск,метална клавиатура,тракбол,контакти" name="keywords">
+<meta name="description" content="Kiosk Bulgaria contact details. Offer request form. Telephone, Fax and E-mail information">
+<meta http-equiv="pragma" content="no-cache">
+<meta http-equiv="cache-control" content="no-cache">
+<link rel="shortcut icon" type="image/x-icon" href="http://www.kioskbg.com/images/icon32x32.ico"/>
+<link rel="icon" type="image/ico" href="http://www.kioskbg.com/images/icon24x24.ico"/>
+<link href="style.css" type="text/css" rel="stylesheet">
+
+<script language="JavaScript" type="text/JavaScript">
+<!--
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+
+function MM_findObj(n, d) { //v4.01
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+//-->
+</script>
+<script language="JavaScript">
+<!--
+function checkMail ( email ) {
+ var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/ ;
+ if ( filter. test ( email ) ) {
+ 	return true ;
+ }
+ return false ;
+} 
+
+function check_it()
+{
+ var complete=true;
+ 
+ var name = document.MAIL_FORM.name.value;
+ var mail = document.MAIL_FORM.email.value;
+ var message = document.MAIL_FORM.message.value;
+ var err = "Не сте попълнили следните полета: \n";
+ 
+ if(name == "") {err += "Име \n"; complete= false;}
+ if(mail == "") {err += "E-mail \n"; complete= false;}
+ if(message == "") {err += "Съобщение"; complete= false;}
+ 
+ if (complete == false)
+ {
+	alert(err);
+ }
+ else  if (!checkMail(mail))
+ {
+   alert ("Въведеният електронен адрес не е валиден!");
+   complete= false;
+ }
+ return complete;
+}
+//-->
+</script>
+</head>
+<body onLoad="MM_preloadImages('images/b1enr.jpg','images/b2enr.jpg','images/b3enr.jpg','images/b4enr.jpg','images/b5enr.jpg','images/b6enr.jpg','images/b7enr.jpg','images/fb_r.png','images/tw_r.png')">
+<table width="996" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td width="821" valign="top"><a href="index.htm"><img src="images/kiosk_bg_logo.png" width="254" height="76" border="0"></a></td>
+    <td width="175" height="76" valign="middle"><div align="center">
+      <table width="110" height="40" border="0" cellpadding="0" cellspacing="0">
+        <tr>
+          <td valign="middle" class="style6"><div align="right"><a href="contact.php">Български</a></div></td>
+        </tr>
+      </table>
+    </div></td>
+  </tr>
+  <tr>
+    <td><a href="index_en.htm" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Home','','images/b1enr.gif',1)"><img src="images/b1enn.gif" alt="Home" name="Home" width="101" height="30" border="0"></a><a href="kiosks_en.htm" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Kiosks','','images/b2enr.gif',1)"><img src="images/b2enn.gif" alt="Kiosks" name="Kiosks" width="101" height="30" border="0"></a><a href="applications_en.htm" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Applications','','images/b3enr.gif',1)"><img src="images/b3enn.gif" alt="Applications" name="Applications" width="138" height="30" border="0"></a><a href="technologies_en.htm" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Technologies','','images/b4enr.gif',1)"><img src="images/b4enn.gif" alt="Technologies" name="Technologies" width="135" height="30" border="0"></a><a href="references_en.htm" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('References','','images/b5enr.gif',1)"><img src="images/b5enn.gif" alt="References" name="References" width="127" height="30" border="0"></a><a href="about_en.htm" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('About','','images/b6enr.gif',1)"><img src="images/b6enn.gif" alt="About" name="About" width="110" height="30" border="0"></a><a href="contact_en.php" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Contact','','images/b7ena.gif',1)"><img src="images/b7ena.gif" alt="Contact" name="Contact" width="109" height="30" border="0"></a></td>
+    <td width="175" rowspan="2" valign="top" background="images/8.gif">
+      <table width="140" height="20" border="0" align="right"  cellpadding="0" cellspacing="0">
+        <tr>
+		<td height="4"></td>
+		</tr>
+		<tr>
+          <td height="25" valign="middle">
+			<form id="form_search" name="form_search" method="get" action="http://www.google.com/search" style="display:inline;">
+			<input name="q" type="text" class="search" id="top_search" size="8"/>
+			<input type="hidden" name="hl" value="" />
+			<input type="hidden" name="as_sitesearch" value="www.kioskbg.com" />
+			<input name="top_search_btn" type="submit" id="top_search_btn" class="search_bg" value="" /></form> 
+		  </td>
+        </tr>
+    </table></td>
+  </tr>
+  <tr valign="top">
+    <td height="14" colspan="2"><img src="images/shadow25_821.png" width="821" height="14"></td>
+  </tr>
+   <tr>
+    <td height="9" colspan="3"><div align="center"><img src="images/white_body_top.gif" width="996" height="9"></div></td>
+  </tr>
+  <tr valign="top" bgcolor="#FFFFFF" background="images/white_body.gif">
+    <td height="424" colspan="3" background="images/white_body.gif"><div align="center">
+	<table width="914" height="68" border="0" cellpadding="0" cellspacing="0">
+        <tr align="left" valign="middle">
+          <td width="42" height="52"></td>
+          <td width="446" class="style4">Contact</td>
+          <td width="426" height="52" class="style4">Offer request</td>
+        </tr>
+      </table>
+	<table width="914" height="434" border="0" align="center" cellpadding="0" cellspacing="0">
+        <tr>
+          <td width="42" height="380" valign="top"><p align="left">&nbsp;</p></td>
+          <td width="432" valign="top" class="style1"><strong><br>
+            Kiosk Bulgaria ltd<br />
+              <br />
+          </strong>VAT BG200204581<br />
+          <br />
+          <br />
+          <strong>Address<br />
+          <br />
+          </strong>Bulgaria, Shumen 9700, Iujen Briag, Bl.3<br />
+          <br />
+          <br />
+          <strong>Telephone<br />
+          <br />
+          </strong>+359 54 88 81 18 <br />
+          <br />
++359 897 90 66 96<br>
+<br>
++359 899 13 13 23<br />
+        <br />
+        <br />
+        <strong>Fax</strong><br />
+        <br />
++359 54 80 18 68 <br />
+<br />
+<br />
+<strong>e-mail<br />
+<br />
+</strong><a href="mailto:office@kioskbg.com">office@kioskbg.com</a></td>
+          <td width="380" valign="top">
+                        <br />
+            <div class="mailform" align="left">
+              <form id="form" action="" method="post" name="MAIL_FORM" onsubmit="return check_it();">
+                <span class="NB"> Your name<span class="style8">*</span></span><br />
+                <input name="name" type="text"  onfocus="if (value=='') value=''" onblur="if (value=='') value=''" size="52" />
+                <br />
+                <span class="NB">Organisation</span><br />
+                <input name="organisation" type="text" onfocus="if (value=='') value=''" onblur="if (value=='') value=''" size="52" />
+                <br />
+                <span class="NB">Telephone</span><br />
+                <input name="telephone" type="text" onfocus="if (value=='') value=''" onblur="if (value=='') value=''" size="52" />
+                <br />
+                <span class="NB">E-mail<span class="style8">*</span></span><br />
+                <input name="email" type="text" class="mailform" onfocus="if (value=='') value=''" onblur="if (value=='') value=''" value="" size="52" />
+                <br />
+                <span class="NB">Massage<span class="style8">*</span> </span><br />
+                <textarea name="message" cols="5" wrap="VIRTUAL" onfocus="if (value=='') value=''" onblur="if (value=='') value=''"></textarea>
+                <br />
+                <br />
+                <center>
+                  <input type="submit" name="MAIL_FORM_SUBMIT" value="Send" />
+                  <input name="reset" type="reset" value="Clear" />
+                </center>
+              </form>
+              <span class="NB"><left></left></span></div>
+                      </td>
+          <td width="60" valign="middle">&nbsp;</td>
+        </tr>
+        <tr>
+          <td height="45" colspan="2" valign="top"></td>
+          <td colspan="2" valign="top">Fields marked with <span class="style8">*</span> are required.</td>
+        </tr>
+      </table>
+    </div></td>
+  </tr>
+  <tr>
+    <td height="9" colspan="3"><img src="images/white_body_bottom.gif" width="996" height="9"></td>
+  </tr>
+  <tr valign="top">
+    <td height="14" colspan="3"><img src="images/shadow.png" width="996" height="12"></td>
+  </tr>
+  <tr valign="middle" background="images/bottom.png">
+    <td height="200" colspan="3" background="images/bottom.png"><table width="940" height="156" border="0" align="center" cellpadding="0" cellspacing="0" class="style7">
+      <tr>
+        <td width="10" class="style7"></td>
+        <td width="206" valign="top" class="style7"><strong><a href="kiosks_en.htm">Kiosks</a></strong></td>
+        <td width="1" valign="top" bgcolor="#999999"></td>
+        <td width="34" rowspan="7" valign="top"></td>
+        <td width="206" valign="top" class="style7"><strong><a href="applications_en.htm">Applications</a></strong></td>
+        <td width="1" valign="top" bgcolor="#999999"></td>
+        <td width="34" rowspan="7" valign="top"></td>
+        <td width="206" valign="top" class="style7"><strong><a href="technologies_en.htm">Technologies</a></strong></td>
+        <td width="1" valign="top" bgcolor="#999999"></td>
+        <td width="35" rowspan="7" valign="top"></td>
+        <td width="206" valign="top" class="style7"><strong><a href="contact_en.php">Contact</a></strong></td>
+      </tr>
+      <tr>
+        <td width="10" height="10" class="style7">&nbsp;</td>
+        <td width="206" valign="top" class="style7">&nbsp;</td>
+        <td width="1" valign="top" bgcolor="#999999"></td>
+        <td width="206" valign="top" class="style7"></td>
+        <td width="1" valign="top" bgcolor="#999999"></td>
+        <td width="206" valign="top" class="style7"></td>
+        <td width="1" valign="top" bgcolor="#999999"></td>
+        <td width="206" valign="top" class="style7"></td>
+      </tr>
+      <tr>
+        <td width="10" class="style7">&nbsp;</td>
+        <td width="206" valign="top" class="style7"><a href="kiosks_mx_series_en.htm">Kiosks MX Series</a></td>
+        <td width="1" valign="top" bgcolor="#999999"></td>
+        <td width="206" valign="top" class="style7"><a href="malls_en.htm">Shopping malls</a></td>
+        <td width="1" valign="top" bgcolor="#999999"></td>
+        <td width="206" valign="top" class="style7"><a href="touchscreen_en.htm">Touch screen monitors</a></td>
+        <td width="1" valign="top" bgcolor="#999999"></td>
+        <td width="206" valign="top" class="style7">Kiosk Bulgaria ltd.</td>
+      </tr>
+      <tr>
+        <td width="10" class="style7">&nbsp;</td>
+        <td width="206" valign="top" class="style7"><a href="kiosks_fx_series_en.htm">Kiosks FX Series</a></td>
+        <td width="1" valign="top" bgcolor="#999999"></td>
+        <td width="206" valign="top" class="style7"><a href="banks_en.htm">Banks</a></td>
+        <td width="1" valign="top" bgcolor="#999999"></td>
+        <td width="206" valign="top" class="style7"><a href="software_en.htm">Kiosk software</a></td>
+        <td width="1" valign="top" bgcolor="#999999"></td>
+        <td width="206" valign="top" class="style7">Shumen 9700, Bulgaria</td>
+      </tr>
+      <tr>
+        <td width="10" class="style7">&nbsp;</td>
+        <td width="206" valign="top" class="style7"><a href="kiosks_ds_series_en.htm">Kiosks DS Series</a></td>
+        <td width="1" valign="top" bgcolor="#999999"></td>
+        <td width="206" valign="top" class="style7"><a href="government_en.htm">Government</a></td>
+        <td width="1" valign="top" bgcolor="#999999"></td>
+        <td width="206" valign="top" class="style7"><a href="metalkeyboard_en.htm">Metal keyboards</a> </td>
+        <td width="1" valign="top" bgcolor="#999999"></td>
+        <td width="206" valign="top" class="style7">Iujen briag, Bl.3</td>
+      </tr>
+      <tr>
+        <td width="10" class="style7">&nbsp;</td>
+        <td width="206" valign="top" class="style7"><a href="kiosks_hawk_series_en.htm">Outdoor kiosks</a></td>
+        <td width="1" valign="top" bgcolor="#999999"></td>
+        <td width="206" valign="top" class="style7"><a href="education_en.htm">Education</a></td>
+        <td width="1" valign="top" bgcolor="#999999"></td>
+        <td width="206" valign="top" class="style7"><a href="printers_en.htm">Printers</a></td>
+        <td width="1" valign="top" bgcolor="#999999"></td>
+        <td width="206" valign="top" class="style7">+359 54 888 118 </td>
+      </tr>
+      <tr>
+        <td width="10" class="style7">&nbsp;</td>
+        <td width="206" valign="top" class="style7"><a href="kiosks_en.htm">more...</a></td>
+        <td width="1" valign="top" bgcolor="#999999"></td>
+        <td width="206" valign="top" class="style7"><a href="applications_en.htm">more...</a></td>
+        <td width="1" valign="top" bgcolor="#999999"></td>
+        <td width="206" valign="top" class="style7"><a href="technologies_en.htm">more...</a></td>
+        <td width="1" valign="top" bgcolor="#999999"></td>
+        <td width="206" valign="top" class="style7"><a href="mailto:office@kioskbg.com">office@kioskbg.com</a></td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr valign="top">
+    <td height="12" colspan="3"><img src="images/shadow.png" width="996" height="12"></td>
+  </tr>
+</table>
+<table width="944" height="35" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td width="64" height="20" valign="middle"><div align="left"><span class="style6">Find us on</span></div></td>
+    <td width="480" height="20" valign="middle"><div align="left"><a href="http://www.facebook.com/KioskBulgaria" target="_blank" title="Find us on Facebook" onmouseover="MM_swapImage('SML1','','images/fb_r.png',1)" onmouseout="MM_swapImgRestore()"><img src="images/fb_n.png" alt="Facebook" name="SML1" width="20" height="20" border="0" id="SML1" /></a>&nbsp;<a href="https://twitter.com/#!/KioskBulgaria" target="_blank" title="Find us on Twitter" onmouseover="MM_swapImage('SML2','','images/tw_r.png',1)" onmouseout="MM_swapImgRestore()"><img src="images/tw_n.png" alt="Twitter" name="SML2" width="20" height="20" border="0" id="SML2" /></a> </div>
+        <div align="right"></div></td>
+    <td width="400" valign="middle"><div align="right" class="style6">2008 - 2017 Kiosk Bulgaria ltd.</div></td>
+  </tr>
+  <tr>
+    <td width="64" height="12" valign="middle"></td>
+    <td height="12" colspan="2"></td>
+  </tr>
+</table>
+</body>
+</html>
